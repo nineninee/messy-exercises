ed6e7912012fad4783b0983cb097e8b588f3c185
wangya <1264178545@qq.com>
2024-09-23 14:29:17 +0800

    结构型设计模式-观察者模式: 时间观察者

--- src/design-pattern/2-13-observer.ts
+++ src/design-pattern/2-13-observer.ts
@@ -1,13 +1,13 @@
-namespace Test {
+namespace TimeObserver {
   // 创建 readline 接口
-  // const rl = require("readline").createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // });
-  // const iter = rl[Symbol.asyncIterator]();
-  // const readline = async () => (await iter.next()).value;
+  const rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const iter = rl[Symbol.asyncIterator]();
+  const readline = async () => (await iter.next()).value;
 
-  // 主题接口-主题
+  // 主题接口
   interface Subject {
     // 注册观察者
     registerObserver(observer: Observer): void;
@@ -19,48 +19,74 @@ namespace Test {
 
   // 观察者接口-观察者
   interface Observer {
-    // 更新方法
+    // 更新信息
     update(message: string): void;
   }
 
-  // 具体主题实现
-  class ConcreteSubject implements Subject {
-    private observers: Array<Observer> = new Array<Observer>()
-
-    private state: string = ''
+  // 具体主题-时钟
+  class Clock implements Subject {
+    private students: Observer[] = [];
+    private time: string = "";
 
     // 注册观察者
     registerObserver(observer: Observer): void {
-      this.observers.push(observer)
+      this.students.push(observer);
     }
 
     // 移除观察者
     removeObserver(observer: Observer): void {
-      const index = this.observers.indexOf(observer)
-      if (index > -1) {
-        this.observers.splice(index, 1)
-      }
+      this.students = this.students.filter((item) => item !== observer);
     }
 
     // 通知观察者
     notifyObserver(): void {
-      this.observers.forEach(observer => {
-        observer.update(this.state)
-      })
+      this.students.forEach((observer) => {
+        observer.update(this.time);
+      });
     }
 
-    // 设置状态
-    setState(state: string): void {
-      this.state = state
-      this.notifyObserver()
+    // 设置时间
+    setTime(time: string): void {
+      this.time = time;
+      this.notifyObserver();
     }
   }
 
-  // 具体观察者实现
-  class ConcreteObserver implements Observer {
-    update(message: string): void {}
+  // 具体观察者-学生
+  class Student implements Observer {
+    private name: string;
+
+    constructor(name: string) {
+      this.name = name;
+    }
+
+    // 更新信息
+    update(message: string): void {
+      console.log(`${this.name} ${message}`);
+    }
   }
 
   // 客户端代码
-  (async () => {})();
+  (async () => {
+    //  创建时钟对象
+    const clock: Clock = new Clock();
+
+    // 读取输入
+    const N = await readline();
+    for (let i = 0; i < N; i++) {
+      const name = await readline();
+
+      // 创建观察者对象
+      const student: Student = new Student(name);
+
+      // 注册观察者
+      clock.registerObserver(student);
+    }
+
+    // 设置时间
+    const M = await readline();
+    for (let i = 0; i < M; i++) {
+      clock.setTime(String(i + 1));
+    }
+  })();
 }
