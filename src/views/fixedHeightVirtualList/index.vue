12ce5d7baef853542806250441d158aafc8d13b6
wangya <1264178545@qq.com>
2025-01-10 17:10:40 +0800

    虚拟列表

---
+++ src/views/fixedHeightVirtualList/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="height: 80%; width: 100%; border: 1px solid #999">
+    <VirtualList :listData="data" :itemSize="100"/>
+  </div>
+</template>
+
+<script setup>
+import VirtualList from "./components/VirtualList.vue";
+const { proxy } = getCurrentInstance();
+
+const data = ref([])
+for(let i = 0; i < 1000; i++){
+  data.value.push({id: i, value: i})
+}
+</script>
+
+<style scoped lang="scss"></style>
